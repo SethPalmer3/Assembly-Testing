f5b380973340c7012aba77aacda10bd700bf80d9
SethPalmer3 <elderlord30@gmail.com>
2021-10-28 22:36:35 -0700

    Actually Finished

--- 4-4.c
+++ 4-4.c
@@ -7,7 +7,7 @@ typedef long array_t[N][N];
 void transpose(array_t a){
 	for (int i = 0; i < N; ++i)
 	{
-		for (int j = 0; j < i; ++i)
+		for (int j = 0; j < i; ++j)
 		{
 			long t1 = a[i][j];
 			long t2 = a[j][i];
@@ -15,4 +15,31 @@ void transpose(array_t a){
 			a[j][i] = t1;
 		}
 	}
+}
+
+// access 2D arrays = (*(a + r) + c)
+// r = row
+// c = col
+
+void opt_transpose(array_t a){
+	long temp = 0;
+	for (int i = 0; i < N; ++i)
+	{
+		long *rp = &a[i][0];
+		long *rx = &a[0][i];
+		for (int j = 0; j < i; ++j)
+		{
+			temp = *(rp);
+			*rp = *rx;
+			*rx = temp;
+			rp += 1;
+			rx += 4;
+		}
+	}
+}
+
+int main(int argc, char const *argv[])
+{
+	//array_t test = {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}
+	return 0;
 }
